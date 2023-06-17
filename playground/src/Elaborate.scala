9dc4d45ea9485c434b35ad0bd35da251436b5d89
Zihao Yu <yuzihao@ict.ac.cn>
2023-06-17 15:43:39 +0800

    bump chisel to 5.0.0

--- playground/src/Elaborate.scala
+++ playground/src/Elaborate.scala
@@ -2,11 +2,6 @@ import circt.stage._
 
 object Elaborate extends App {
   def top = new GCD()
-  val useMFC = true // use MLIR-based firrtl compiler
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  if (useMFC) {
-    (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
-  } else {
-    (new chisel3.stage.ChiselStage).execute(args, generator)
-  }
+  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
 }
